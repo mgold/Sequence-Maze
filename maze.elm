658481a0d15bb5f69d5b57161136b9879e61e289
Max Goldstein <maxgoldstein1@gmail.com>
2013-10-28 18:18:30 -0400

    Can change player starting position.
    Done by not keeping Form around until draw time.

--- maze.elm
+++ maze.elm
@@ -1,20 +1,23 @@
 import Window
 import Http (sendGet, Success, Response)
-import Dict
+import Set
 import Keyboard (arrows)
 
 data Move = Up | Down | Left | Right
 type Frac = Float -- semantically between 0 and 1
 type Movement = Maybe (Move, Frac)
+type Coord = (Int, Int)
 
 moves : Signal (Maybe Move)
-moves = let toMove : { x:Int, y:Int } -> Maybe Move
-            toMove {x,y} = case (x,y) of
-                    (0, 0) -> Nothing
-                    (x, 0) -> if x == 1 then Just Right else Just Left
-                    (0, y) -> if y == 1 then Just Up else Just Down
+moves = let
+    toMove : {x:Int, y:Int} -> Maybe Move
+    toMove {x,y} = case (x,y) of
+                    (-1, 0) -> Just Left
+                    ( 1, 0) -> Just Right
+                    ( 0,-1) -> Just Down
+                    ( 0, 1) -> Just Up
                     _ -> Nothing
-      in lift toMove arrows |> keepIf isJust Nothing
+        in lift toMove arrows |> keepIf isJust Nothing
 
 
 movement : Signal Movement
@@ -25,20 +28,26 @@ movement = let toMovement m = case m of
 
 
 type Player = {i : Int, j : Int,
-               dx : Float, dy : Float,
-               form : Form}
+               dx : Float, dy : Float
+              }
 
+playerBase (i,j) = Player i j 0 0
 
-playerBase = Player 0 0 0 0 (square 30 |> filled black |> rotate (degrees 45))
+playerForm : Form
+playerForm = square 30 |> filled black |> rotate (degrees 45)
 
 type Level = {number: Int,
               s: Int,
               w: Int,
               h: Int,
-              obstacles: Dict.Dict (Int,Int) Bool
+              start: Coord,
+              obstacles: Set.Set Coord
+              --seq: [Move]
+              --goal : Coord
               }
 
-level1 = Level 1 100 4 8 Dict.empty
+level1 = Level 1 100 5 4 (4,3) (Set.fromList [(1,0),(1,1),(1,2), (3,3),(3,2),(3,1)])
+               --[Up, Up, 
 level : Signal Level
 level = constant level1
 
@@ -50,39 +59,36 @@ update = merge uLevel uMoves
 
 type State = (Level, Player)
 state0 : State
-state0 = (level1, playerBase)
+state0 = (level1, playerBase level1.start)
 
 stepFun : Update -> State -> State
 stepFun u s = case u of
     UMove m -> if okMove m s then doMove m s else s
-    ULevel l -> (l, playerBase)
+    ULevel l -> (l, playerBase level1.start)
 
 state = foldp stepFun state0 update
 
 
 okMove m (lvl, plyr) = case m of
     Right -> lvl.w > plyr.i +1
+                && not (Set.member (plyr.i+1, plyr.j) lvl.obstacles)
     Left -> plyr.i > 0
+                && not (Set.member (plyr.i-1, plyr.j) lvl.obstacles)
     Up -> lvl.h > plyr.j +1
+                && not (Set.member (plyr.i, plyr.j+1) lvl.obstacles)
     Down -> plyr.j > 0
+                && not (Set.member (plyr.i, plyr.j-1) lvl.obstacles)
 
--- Elm does not support nested record updates
 doMove : Move -> State -> State
-doMove m (lvl, p) = let
-    s = toFloat lvl.s
-    frm0 = p.form
-    frm = case m of
-        Right -> {frm0|x <- frm0.x + s}
-        Left  -> {frm0|x <- frm0.x - s}
-        Up    -> {frm0|y <- frm0.y + s}
-        Down  -> {frm0|y <- frm0.y - s}
-    plyr = case m of
-        Right -> {p|i <- p.i + 1}
-        Left  -> {p|i <- p.i - 1}
-        Up    -> {p|j <- p.j + 1}
-        Down  -> {p|j <- p.j - 1}
-            in (lvl, {plyr|form <- frm})
+doMove m (lv, p) = (lv, case m of
+    Right -> {p|i <- p.i + 1}
+    Left  -> {p|i <- p.i - 1}
+    Up    -> {p|j <- p.j + 1}
+    Down  -> {p|j <- p.j - 1}
+        )
 
+drawPlayer : State -> Form
+drawPlayer (lv, p) = playerForm |> move (toFloat (lv.s*p.i), toFloat (lv.s*p.j))
 
 stats : Signal Element
 stats = flow <~ constant down ~ combine
@@ -109,7 +115,11 @@ scene (w,h) (lv,p) = let
                                           |> move (s*toFloat i, s*toFloat j)))
                 (pairs [0..lv.w-1] [0..lv.h-1])
             |> group |> center
-        , p.form |> center
+        , map (\(i,j) -> (square (0.9*s) |> filled darkCharcoal
+                                         |> move (s*toFloat i, s*toFloat j)))
+                (Set.toList lv.obstacles)
+            |> group |> center
+        , drawPlayer (lv,p) |> center
         ]
 
 main = layers <~ combine
