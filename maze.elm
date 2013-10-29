2272a14afeeb61081e477e6558cc6587b75a0aa8
Max Goldstein <maxgoldstein1@gmail.com>
2013-10-28 22:34:30 -0400

    Display sequence arrows.

--- maze.elm
+++ maze.elm
@@ -39,19 +39,28 @@ playerForm = square 30 |> filled black |> rotate (degrees 45)
 goalForm : Form
 goalForm = square 50 |> filled yellow
 
+arrowForm : Move -> Form
+arrowForm m = case m of
+    Right -> ngon 3 30 |> filled green
+    Up ->    ngon 3 30 |> filled blue   |> rotate (turns 0.25)
+    Left ->  ngon 3 30 |> filled purple |> rotate (turns 0.5)
+    Down ->  ngon 3 30 |> filled red    |> rotate (turns 0.75)
+
 type Level = {number: Int,
               s: Int,
               w: Int,
               h: Int,
               start: Coord,
               goal : Coord,
-              obstacles: Set.Set Coord
-              --seq: [Move]
+              obstacles: Set.Set Coord,
+              seq: [(Coord, Move)]
               }
 
 level1 = Level 1 100 5 4 (0,0) (4,3)
             (Set.fromList [(1,0),(1,1),(1,2), (3,3),(3,2),(3,1)])
-               --[Up, Up, 
+
+ [((0,1), Up), ((0,2), Up), ((0,3), Right), ((1,3), Right), ((2,3), Down)]
+
 level : Signal Level
 level = constant level1
 
@@ -71,7 +80,6 @@ stepFun u s = case u of
     ULevel l -> (l, playerBase level1.start)
 
 state = foldp stepFun state0 update
-
 
 okMove m (lvl, plyr) = case m of
     Right -> lvl.w > plyr.i +1
@@ -97,6 +105,9 @@ drawPlayer (lv, p) = playerForm |> move (toFloat (lv.s*p.i), toFloat (lv.s*p.j))
 drawGoal : Level -> Form
 drawGoal lv = move (both (\i -> toFloat <| lv.s*i) lv.goal) goalForm
 
+drawArrow : Int -> (Coord, Move) -> Form
+drawArrow s (c, m) = move (both (\i -> toFloat <| s*i) c) (arrowForm m)
+
 stats : Signal Element
 stats = flow <~ constant down ~ combine
     [ (\lv -> (text . monospace . toText) (show lv.w++"x"++show lv.h++" @"++show lv.s))
@@ -107,16 +118,11 @@ stats = flow <~ constant down ~ combine
         <~ lift snd state
     ]
 
--- because Elm doesn't have list comprehensions
-pairs xs ys = let n = length ys in case xs of
-    [] -> []
-    (x::tl) -> (zip (map (\_->x) [1..n]) ys) ++ pairs tl ys
-
 scene : (Int,Int) -> State -> Element
 scene (w,h) (lv,p) = let
     s = toFloat lv.s
     center = move (-s * toFloat (lv.w-1) * 0.5, -s * toFloat (lv.h-1) * 0.5)
-        in collage w h
+        in collage w h (
         [ rect (toFloat w) (toFloat h) |> filled lightCharcoal
         , map (\(i,j) -> (square (0.99*s) |> filled white
                                           |> move (s*toFloat i, s*toFloat j)))
@@ -126,9 +132,11 @@ scene (w,h) (lv,p) = let
                                          |> move (s*toFloat i, s*toFloat j)))
                 (Set.toList lv.obstacles)
             |> group |> center
-        , drawGoal lv |> center
+        ] ++ (map (center . drawArrow lv.s) lv.seq) ++
+        [ drawGoal lv |> center
         , drawPlayer (lv,p) |> center
         ]
+        )
 
 main = layers <~ combine
            [ scene <~ Window.dimensions ~ state
@@ -141,3 +149,9 @@ movePolar (r, theta) = move <| fromPolar (r, theta)
 
 both : (a -> b) -> (a, a) -> (b, b)
 both f (m, n) = (f m, f n)
+
+-- because Elm doesn't have list comprehensions
+pairs xs ys = let n = length ys in case xs of
+    [] -> []
+    (x::tl) -> (zip (map (\_->x) [1..n]) ys) ++ pairs tl ys
+
